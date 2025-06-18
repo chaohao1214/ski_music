5b5b7833213fdb3ef21fcd990ec1cfca35d08b76
Chaohao Zhu <chaohaozhu@Mac-mini.local>
2025-06-18 15:41:42 -0400

    update server.js added

--- server/server.js
+++ server/server.js
@@ -1,20 +1,25 @@
-import express, { json } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
 import cors from "cors";
+import { initDb } from "./src/services/databaseService.js";
+import authRoutes from "./src/api/authRoutes.js";
+import playlistRoutes from "./src/api/playlistRoutes.js";
+
+//initialize database
+initDb();
 
 const app = express();
-const PORT = process.env.PORT || "3001";
 
+// middleware
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/playlist", (req, res) => {
-  const dummyPlaylist = [
-    { id: 1, title: "First Dance (ESM Version)", duration: 185, url: "..." },
-    { id: 2, title: "Skating Dream (ESM Version)", duration: 210, url: "..." },
-  ];
-  res.json(dummyPlaylist);
-});
+app.use("/api/playlist", playlistRoutes);
+app.use("/api/auth", authRoutes);
 
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`🚀 Server with ES Modules running on http://localhost:${PORT}`);
 });
